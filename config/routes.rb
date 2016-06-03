4045e40345ce8934e38e252a347443ca66f2a67f
softcomet <davesoft110@gmail.com>
2016-06-03 14:36:19 +0200

    Fixed the signout issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :users
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy' 
     authenticated :user do
       root :to => 'visitors#index', as: :authenticated_root
     end
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
       root :to => 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+  
+  resources :users
 end
