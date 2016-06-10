75c185e27134d319e4bb6bac2f141075e57438ee
softcomet <davesoft110@gmail.com>
2016-06-10 21:37:40 +0200

    Changed the domain name for sendgrid mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: "aerotage.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
