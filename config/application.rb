cf4430699258480521d3cb93202e489afaef0f7c
softcomet <davesoft110@gmail.com>
2016-06-10 16:51:51 +0200

    Fix the conversion issue from bignum into string

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Aerotage
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'application.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+        ENV[key.to_s] = value.to_s
       end if File.exists?(env_file)
     end
   end
