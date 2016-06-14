9933b8b17f19f6b95ae1eb5805e6f2ebab31e61e
softcomet <davesoft110@gmail.com>
2016-06-14 19:18:18 +0200

    updated with mailgun for devise action mailer

--- Gemfile
+++ Gemfile
@@ -61,3 +61,4 @@ end
 
 # Memory Caching for performance
 gem 'dalli' 
+gem 'mailgun'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    address: "smtp.mailgun.org",
     port: 587,
     domain: "aerotage.com",
     authentication: "plain",
