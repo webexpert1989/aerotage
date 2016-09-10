28f9a58fb02931b2967ee2b61f247c6749762e39
softcomet <davesoft110@gmail.com>
2016-09-10 07:33:43 +0200

    Chagned the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :admins
   root 'static_pages#home'
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations:"registrations", sessions:"sessions" }
 
   devise_scope :user do
